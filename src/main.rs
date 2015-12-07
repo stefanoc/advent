37ee4fbb83a5cc25a0206891385e56855cde7ab0
Stefano Cobianchi <stefano@macpro-046.homenet.telecomitalia.it>
2015-12-07 18:46:22 +0100

    Days 1-2

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod one;
+mod two;
+mod three;
+
+fn main() {
+    println!("Day 1: {:?}", one::solve(one::INPUT));
+    println!("Day 2: {:?}", two::solve(two::INPUT));
+    println!("Day 3: {:?}", three::solve(three::INPUT));
+
+}
