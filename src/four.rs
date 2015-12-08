34ec632c504fd3c320c243d6f17ed388672b4e68
Stefano Cobianchi <stefano.cobianchi@me.com>
2015-12-08 13:01:44 +0100

    Day 4

---
+++ src/four.rs
@@ -0,0 +1,20 @@
+extern crate md5;
+
+pub static INPUT: &'static str = "ckczppom";
+
+macro_rules! md5(
+    ($string:expr) => ({
+        let mut context = md5::Context::new();
+        context.consume($string.as_bytes());
+        let mut digest = String::with_capacity(2 * 16);
+        for x in &context.compute()[..] {
+            digest.push_str(&format!("{:02x}", x));
+        }
+        digest
+    });
+);
+
+pub fn solve(input: &str) -> (usize, usize) {
+    let test = |n, pat| md5!(format!("{}{}", input, n)).starts_with(pat);
+    ((0..).find(|&n| test(n, "00000")).unwrap(), (0..).find(|&n| test(n, "000000")).unwrap())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod one;
 mod two;
 mod three;
+mod four;
 
 fn main() {
     println!("Day 1: {:?}", one::solve(one::INPUT));
     println!("Day 2: {:?}", two::solve(two::INPUT));
     println!("Day 3: {:?}", three::solve(three::INPUT));
+    println!("Day 4: {:?}", four::solve(four::INPUT));
 }
