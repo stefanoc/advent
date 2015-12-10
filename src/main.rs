90e9779eae9bfe7ed6b01b66d1ef02a81c2f236c
Stefano Cobianchi <stefano.cobianchi@me.com>
2015-12-10 17:20:14 +0100

    Day 5, part 1

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod one;
 mod two;
 mod three;
 mod four;
+mod five;
 
 fn main() {
     println!("Day 1: {:?}", one::solve(one::INPUT));
     println!("Day 2: {:?}", two::solve(two::INPUT));
     println!("Day 3: {:?}", three::solve(three::INPUT));
     println!("Day 4: {:?}", four::solve(four::INPUT));
+    println!("Day 5: {:?}", five::solve(five::INPUT));
 }
