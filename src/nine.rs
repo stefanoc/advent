9ea450c31bc467362ae27f0addf03b275a47e8e4
Stefano Cobianchi <stefano.cobianchi@me.com>
2016-03-17 13:23:04 +0100

    Day 9 (part 2)

--- src/nine.rs
+++ src/nine.rs
@@ -2,51 +2,65 @@ use std::collections::HashMap;
 use std::convert::From;
 use permutohedron::LexicalPermutation;
 
-pub fn solve(input: &str) -> usize {
+pub fn solve(input: &str) -> (usize, usize) {
     let routes: Vec<_> = input.lines().map(Route::from).collect();
     let mut map = Map::new();
     for r in &routes {
-        {
-            let mut data = map.entry(r.from).or_insert(Distances::new());
-            data.insert(r.to, r.dist);
-        }
-        {
-            let mut data = map.entry(r.to).or_insert(Distances::new());
-            data.insert(r.from, r.dist);
-        }
+        map.add_distance(r.from, r.to,   r.dist);
+        map.add_distance(r.to,   r.from, r.dist);
     }
-    let mut cities: Vec<_> = map.keys().map(|val| *val).collect();
+    let mut cities = map.cities();
     let mut trips = vec![];
     while cities.next_permutation() {
-        if let Some(dist) = trip_distance(&map, &cities) {
+        if let Some(dist) = map.trip_distance(&cities) {
             trips.push(dist);
         }
     }
     trips.sort();
-    trips[0]
+    (trips[0], trips[trips.len() - 1])
 }
 
 type Distances<'a> = HashMap<&'a str, usize>;
-type Map<'a> = HashMap<&'a str, Distances<'a>>;
+// type Map<'a> = HashMap<&'a str, Distances<'a>>;
 
-fn trip_distance(map: &Map, trip: &Vec<&str>) -> Option<usize> {
-    let mut iter = trip.iter().peekable();
-    let mut dist = 0;
-    while let Some(city) = iter.next() {
-        if let Some(next_city) = iter.peek() {
-            if let Some(val) = distance(&map, city, next_city) {
-                dist += val;
-            } else {
-                return None;
+struct Map<'a> {
+    data: HashMap<&'a str, Distances<'a>>
+}
+
+impl<'a> Map<'a> {
+    fn new() -> Map<'a> {
+        Map { data: HashMap::<&str, Distances>::new() }
+    }
+
+    fn cities(&self) -> Vec<&str> {
+        self.data.keys().map(|val| *val).collect()
+    }
+
+    fn add_distance(&mut self, from: &'a str, to: &'a str, dist: usize) {
+        let mut entry = self.data.entry(from).or_insert(Distances::new());
+        entry.insert(to, dist);
+    }
+
+    fn trip_distance(&self, trip: &Vec<&str>) -> Option<usize> {
+        let mut iter = trip.iter().peekable();
+        let mut dist = 0;
+        while let Some(city) = iter.next() {
+            if let Some(next_city) = iter.peek() {
+                if let Some(val) = self.distance(city, next_city) {
+                    dist += val;
+                } else {
+                    return None;
+                }
             }
         }
+        return Some(dist);
     }
-    return Some(dist);
+
+    fn distance(&self, from: &str, to: &str) -> Option<usize> {
+        self.data.get(from).and_then(|data| data.get(to).map(|val| *val))
+    }
 }
 
-fn distance(map: &Map, from: &str, to: &str) -> Option<usize> {
-    map.get(from).and_then(|data| data.get(to).map(|val| *val))
-}
 
 #[derive(Debug, Clone)]
 struct Route<'a> {
